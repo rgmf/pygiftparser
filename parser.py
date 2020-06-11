310aa08e3d7299710e17177cb6c6f9601a6ff09e
Román <rgmf@riseup.net>
2020-06-11 08:54:46 +0200

    Parse files with comments and questions with and without question name.

--- parser.py
+++ parser.py
@@ -1,3 +1,4 @@
+import argparse
 from ply import lex
 import ply.yacc as yacc
 
@@ -7,7 +8,7 @@ import ply.yacc as yacc
 # List of token names.
 tokens = (
     'SLASH',
-    'COLON',
+    'COLONCOLON',
     'OPEN_BRACE',
     'CLOSE_BRACE',
     'NEWLINE',
@@ -16,7 +17,7 @@ tokens = (
 )
 
 # Regular expression rules for simple tokens.
-t_COLON       = r':'
+t_COLONCOLON       = r'::'
 t_OPEN_BRACE  = r'{'
 t_CLOSE_BRACE = r'}'
 t_SCAPE       = r'\\.'
@@ -50,29 +51,53 @@ lexer = lex.lex()
 
 def p_expression_gift_file(p):
     """
+    gift_file : optional_newline
     gift_file : NEWLINE gift_file
-    gift_file : question_name
-    gift_file : question_name endquestion gift_file
+    gift_file : question_expr
+    gift_file : question_expr endquestion gift_file
     """
-    if len(p) == 2:
+    if len(p) == 2 and p[1] != 'NEWLINE':
+        p[0] = ''
+    elif len(p) == 2 and p[1] == 'NEWLINE':
         p[0] = p[1]
     elif len(p) == 3:
         p[0] = p[2]
-    elif len(p) == 4:
+    elif len(p) == 4 and p[3] != '':
         p[0] = p[1] + '\n' + p[3]
+    elif len(p) == 4 and p[3] == '':
+        p[0] = p[1]
 
-def p_expression_question_name(p):
+
+def p_expression_question_expr(p):
     """
-    question_name : COLON COLON string COLON COLON question
-    question_name : COLON COLON string COLON COLON question OPEN_BRACE answer CLOSE_BRACE
-    question_name : COLON COLON string COLON COLON question OPEN_BRACE NEWLINE answer NEWLINE CLOSE_BRACE
+    question_expr : question_noname_expr
+    question_expr : question_withname_expr
     """
-    if len(p) == 7:
-        p[0] = p[3] + ": " + p[6]
+    p[0] = p[1]
+
+
+def p_expression_question_withname_expr(p):
+    """
+    question_withname_expr : COLONCOLON string COLONCOLON question
+    question_withname_expr : COLONCOLON string COLONCOLON question OPEN_BRACE optional_newline CLOSE_BRACE
+    question_withname_expr : COLONCOLON string COLONCOLON question OPEN_BRACE optional_newline answer optional_newline CLOSE_BRACE
+    """
+    if len(p) == 5 or len(p) == 8:
+        p[0] = p[2] + ": " + p[4]
     elif len(p) == 10:
-        p[0] = p[3] + ". " + p[6] + "(" + p[8] + ")"
-    elif len(p) == 12:
-        p[0] = p[3] + ". " + p[6] + "(" + p[9] + ")"
+        p[0] = p[2] + ": " + p[4] + "(" + p[7] + ")"
+
+
+def p_expression_question_noname_expr(p):
+    """
+    question_noname_expr : question
+    question_noname_expr : question OPEN_BRACE optional_newline CLOSE_BRACE
+    question_noname_expr : question OPEN_BRACE optional_newline answer optional_newline CLOSE_BRACE
+    """
+    if len(p) == 2 or len(p) == 5:
+        p[0] = p[1]
+    elif len(p) == 7:
+        p[0] = p[1] + "(" + p[4] + ")"
 
 
 def p_expression_string(p):
@@ -85,11 +110,13 @@ def p_expression_string(p):
     elif len(p) == 3:
         p[0] = p[1] + p[2]
 
+
 def p_expression_question(p):
     """
     question : string
     """
     p[0] = p[1]
+
 
 def p_expression_answer(p):
     """
@@ -101,8 +128,17 @@ def p_expression_answer(p):
     elif len(p) == 4:
         p[0] = p[1] + ", " + p[3]
 
+
 def p_expression_endquestion(p):
     'endquestion : NEWLINE NEWLINE'
+
+
+def p_expression_optional_newline(p):
+    """
+    optional_newline :
+    optional_newline : NEWLINE optional_newline
+    """
+
 
 # Error rule for syntax errors
 def p_error(p):
@@ -111,10 +147,24 @@ def p_error(p):
         raise Exception(f"Syntax error at '{p.value}' in line {p.lineno}.")
     else:
         print('Syntax error at EOI')
-        raise Exception('Syntax error at EOI: ' + p)
+        raise Exception('Syntax error at EOI: ')
+
 
 parser = yacc.yacc()
-with open('file.gift', 'r') as myfile:
+
+
+##############################################################################
+
+def parse_input_arguments():
+    parser = argparse.ArgumentParser(description='Stats generator about subjects sessions.')
+    parser.add_argument('-f', '--file', dest='file', required=True,
+                        help='GIFT Moodle file.')
+    args = parser.parse_args()
+    return args
+
+
+args = parse_input_arguments()
+with open(args.file, 'r') as myfile:
   s = myfile.read()
   #print('FICHERO')
   #print(s)
