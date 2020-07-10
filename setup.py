c44ea1c8a3c5d5864c7da2e693d5803c27141adf
Román <rgmf@riseup.net>
2020-07-10 16:16:00 +0200

    Added setup file.

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pygiftparserrgmf',
+    version='0.0.1',
+    author='Román Ginés Martínez Ferrández',
+    author_email='rgmf@riseup.net',
+    description='Moodle GIFT files parser',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/rgmf/pygiftparser',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: POSIX :: Linux'
+    ],
+    python_requires='>=3.8',
+)
